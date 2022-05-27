9fd337606b7c57186a5804dea2c3fabb3e43419d
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-27 18:08:15 +1000

    commit tree 72a7c63b37a195d16f0d447a85c9efceb6968caa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName (){
+   customerName = customerName.toUpperCase();
+}
+function setBestCustomer (){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer;
+}
+
+const leastFavouriteCustomer = 'Nick';
+function changeLeastFavoriteCustomer (customer) {
+    leastFavouriteCustomer = customer;
+}
